a8dcf4b8434523ed7c2cd23483cdda398c882bcd
Jacob Broughton <jlbroughton88@gmail.com>
2024-09-02 18:14:31 -0400

    added crude properties window for each type

--- src/components/pages/CreateForm/CreateForm.tsx
+++ src/components/pages/CreateForm/CreateForm.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import "./CreateForm.css";
+import { FormItemTypeType, FormItemTypePropertyType } from "../../../lib/types";
 
 const CreateForm = () => {
   const [formItems, setFormItems] = useState([]);
-  const [formItemTypes, setFormItemTypes] = useState([]);
-  const [formItemTypesSelectorOpen, setFormItemTypesSelectorOpen] = useState(false);
+  const [formItemTypes, setFormItemTypes] = useState<FormItemTypeType[]>([]);
+  const [formItemTypesSelectorOpen, setFormItemTypesSelectorOpen] =
+    useState<boolean>(false);
+  const [stagedNewFormItemType, setStagedNewFormItemType] =
+    useState<FormItemTypeType | null>(null);
+  const [formItemTypeProperties, setFormItemTypeProperties] = useState<
+    FormItemTypePropertyType[]
+  >([]);
 
   async function addNewFormItem(): Promise<void> {
     try {
@@ -20,13 +27,34 @@ const CreateForm = () => {
 
   async function getFormItemTypes(): Promise<void> {
     try {
-      const response = await fetch("http://localhost:3001/form-item-types");
+      const response = await fetch("http://localhost:3001/form/item-types");
 
       if (!response.ok) throw new Error("An error occured while fetching form types");
 
       const data = await response.json();
 
-      setFormItemTypes(data)
+      setFormItemTypes(data);
+
+      console.log(data);
+    } catch (error) {
+      if (typeof error === "string") {
+        console.log(error.toUpperCase()); // works, `e` narrowed to string
+      } else if (error instanceof Error) {
+        console.log(error.message); // works, `e` narrowed to Error
+      }
+    }
+  }
+
+  async function getFormItemTypeProperties(): Promise<void> {
+    try {
+      const response = await fetch("http://localhost:3001/form/item-type-properties");
+
+      if (!response.ok)
+        throw new Error("An error occured while fetching form item type properties");
+
+      const data = await response.json();
+
+      setFormItemTypeProperties(data);
 
       console.log(data);
     } catch (error) {
@@ -40,21 +68,54 @@ const CreateForm = () => {
 
   useEffect(() => {
     getFormItemTypes();
+    getFormItemTypeProperties();
   }, []);
 
+  console.log({ formItemTypeProperties, stagedNewFormItemType });
+
   return (
-    <main>
+    <main className="create-form">
       <h1>Create Form</h1>
 
       <form>
         {formItemTypesSelectorOpen && formItemTypes.length ? (
-          <div>
+          <ul className="form-item-types">
             {formItemTypes.map((formItemType) => (
-              <button type="button">{formItemType.name}</button>
+              <>
+                <button
+                  className={`${
+                    stagedNewFormItemType?.id === formItemType.id ? "selected" : ""
+                  }`}
+                  type="button"
+                  onClick={() => setStagedNewFormItemType(formItemType)}
+                >
+                  <p className="name">{formItemType.name}</p>
+                  <p className="description">{formItemType.description}</p>
+                </button>
+                {formItemTypeProperties[formItemType.id].length &&
+                stagedNewFormItemType?.id === formItemType.id ? (
+                  <div className="properties">
+                    <p>Properties</p>
+                    {console.log(formItemTypeProperties[formItemType.id])}
+                    {formItemTypeProperties[formItemType.id].map((itemTypeProperty) => (
+                      <div>{itemTypeProperty.property_name}</div>
+                    ))}
+                  </div>
+                ) : (
+                  false
+                )}
+              </>
             ))}
-          </div>
+            <button type="button" onClick={() => setFormItemTypesSelectorOpen(false)}>
+              Cancel
+            </button>
+          </ul>
         ) : (
-          <button type="button" onClick={() => setFormItemTypesSelectorOpen(true)}>
+          <button
+            className="add-new-form-item"
+            type="button"
+            onClick={() => setFormItemTypesSelectorOpen(true)}
+          >
             Add new form item
           </button>
         )}
---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+export type FormItemTypeType = {
+  id: number,
+  name: string,
+  description: string
+}
+
+export type FormItemTypePropertyType = {
+  id: number,
+  data_type_id: number,
+	property_name: string,
+	property_description: string
+}
